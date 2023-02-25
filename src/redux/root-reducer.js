59ddef95dccde43efa1d6a089b59d5c0cf9a48a9
serhii95zhydkov <s.o.zhidkov@gmail.com>
2023-02-25 12:35:12 +0200

    Create root-reducer.js

---
+++ src/redux/root-reducer.js
@@ -0,0 +1,21 @@
+import { combineReducers } from '@reduxjs/toolkit';
+
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import { contactsReducer } from './contacts/contacts-slice';
+import { filterReducer } from './filter/filter-slice';
+
+const rootReducer = combineReducers({
+  contacts: contactsReducer,
+  filter: filterReducer,
+});
+
+const persistConfig = {
+  key: 'my-contacts',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export default persistedReducer;
